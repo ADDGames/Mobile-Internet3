bc979a7b8ce9f70778ab3fe536758b4dd612f9a3
ADDGames <aikodedoncker@hotmail.com>
2017-12-04 16:01:12 +0100

    x

--- php/users.php
+++ php/users.php
@@ -4,12 +4,21 @@
     $docenten = [];
 
     //docenten
-    $query = "SELECT docent.DOC_id,docent.DOC_naam,docent.DOC_GEB_id,gebruiker.GEB_username,gebruiker.GEB_naam,gebruiker.GEB_voornaam,gebruiker.GEB_wachtwoord,gebruiker.GEB_email FROM docent INNER JOIN gebruiker ON Docent.DOC_gebruiker_id = gebruiker.GEB_id";
-	$result = mysql_query($query);
-	public function set_docenten_database() {
+	public function get_docenten_database() {
+        $query = "SELECT docent.DOC_id,docent.DOC_naam,docent.DOC_GEB_id,gebruiker.GEB_username,gebruiker.GEB_naam,gebruiker.GEB_voornaam,gebruiker.GEB_wachtwoord,gebruiker.GEB_email FROM docent INNER JOIN gebruiker ON Docent.DOC_gebruiker_id = gebruiker.GEB_id";
+        $result = mysql_query($query);
         while($docent = mysql_fetch_array($result)) {
             array_push($docenten, new docent($docent['DOC_id'],$docent['DOC_naam'],$docent['DOC_GEB_id'],$docent['GEB_username'],$docent['GEB_naam'],$docent['GEB_voornaam'],$docent['GEB_wachtwoord'],$docent['GEB_email']));
         }
+        echo json_encode($docenten);
+    }
+	public function add_docent($newDOC_id,$new_DOC_naam,$new_DOC_GEB_id,$new_GEB_username,$new_GEB_naam,$new_GEB_voornaam,$new_GEB_wachtwoord,$new_GEB_email) {
+        $query = "INSERT INTO `gebruiker`(`GEB_id`, `GEB_username`, `GEB_naam`, `GEB_voornaam`, `GEB_wachtwoord`, `GEB_email`) VALUES ($new_DOC_GEB_id,`$new_GEB_username`,`$new_GEB_naam`,`$new_GEB_voornaam`,`$new_GEB_wachtwoord`,`$new_GEB_email`)"
+        $con->query($query);
+        $query = "INSERT INTO `docent`(`DOC_id`, `DOC_naam`, `DOC_gebruiker_id`) VALUES ($newDOC_id,`$new_DOC_naam`,$new_DOC_GEB_id,)"
+        $con->query($query);
+        $docenten = [];
+        get_docenten_database();
     }
     class docent {
 		private $DOC_id;
@@ -20,7 +29,6 @@
         private $GEB_voornaam;
         private $GEB_wachtwoord;
         private $GEB_email;
-
         public function __construct($newDOC_id,$new_DOC_naam,$new_DOC_GEB_id,$new_GEB_username,$new_GEB_naam,$new_GEB_voornaam,$new_GEB_wachtwoord,$new_GEB_email) {
            	$this->DOC_id = $new_DOC_id;
             $this->DOC_naam = $new_DOC_naam;
@@ -31,7 +39,6 @@
             $this->GEB_wachtwoord = $new_GEB_wachtwoord;
             $this->GEB_email = $new_GEB_email;
         }
-
         function get_DOC_id() {
 			return $this->DOC_id;
 		}
@@ -56,7 +63,6 @@
         function get_GEB_email() {
             return $this->GEB_email;
         }
-
         function set_DOC_id($new_DOC_id) {
        		$this->DOC_id = $new_DOC_id;
         }
@@ -82,6 +88,4 @@
             $this->GEB_email = $new_GEB_email;
         }
 	}
-
-
 ?>
