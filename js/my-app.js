e0cba93fe43dcb238e1950eb92a43be56dbd18b4
ADDGames <aikodedoncker@hotmail.com>
2017-12-11 12:03:38 +0100

    x

--- js/my-app.js
+++ js/my-app.js
@@ -1,74 +1,63 @@
 // Initialize your app
 var myApp = new Framework7();
-
 // Export selectors engine
 var $$ = Dom7;
-
 // Add view
 var mainView = myApp.addView('.view-main', {
-    // Because we use fixed-through navbar we can enable dynamic navbar
-    dynamicNavbar: true
+	// Because we use fixed-through navbar we can enable dynamic navbar
+	dynamicNavbar: true
 });
-
 // Callbacks to run specific code for specific pages, for example for About page:
 myApp.onPageInit('about', function (page) {
-    // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        createContentPage();
-    });
+	// run createContentPage func after link was clicked
+	$$('.create-page').on('click', function () {
+		createContentPage();
+	});
 });
-
 // Generate dynamic page
 var dynamicPageIndex = 0;
 
 function createContentPage() {
-    mainView.router.loadContent(
-        '<!-- Top Navbar-->' +
-        '<div class="navbar">' +
-        '  <div class="navbar-inner">' +
-        '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' +
-        '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' +
-        '  </div>' +
-        '</div>' +
-        '<div class="pages">' +
-        '  <!-- Page, data-page contains page name-->' +
-        '  <div data-page="dynamic-pages" class="page">' +
-        '    <!-- Scrollable page content-->' +
-        '    <div class="page-content">' +
-        '      <div class="content-block">' +
-        '        <div class="content-block-inner">' +
-        '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' +
-        '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' +
-        '        </div>' +
-        '      </div>' +
-        '    </div>' +
-        '  </div>' +
-        '</div>'
-    );
-    return;
+	mainView.router.loadContent('<!-- Top Navbar-->' + '<div class="navbar">' + '  <div class="navbar-inner">' + '    <div class="left"><a href="#" class="back link"><i class="icon icon-back"></i><span>Back</span></a></div>' + '    <div class="center sliding">Dynamic Page ' + (++dynamicPageIndex) + '</div>' + '  </div>' + '</div>' + '<div class="pages">' + '  <!-- Page, data-page contains page name-->' + '  <div data-page="dynamic-pages" class="page">' + '    <!-- Scrollable page content-->' + '    <div class="page-content">' + '      <div class="content-block">' + '        <div class="content-block-inner">' + '          <p>Here is a dynamic page created on ' + new Date() + ' !</p>' + '          <p>Go <a href="#" class="back">back</a> or go to <a href="services.html">Services</a>.</p>' + '        </div>' + '      </div>' + '    </div>' + '  </div>' + '</div>');
+	return;
 }
-
 $$('.form-to-data').on('click', function () {
-    var formData = myApp.formToData('#vraag_form');
-    alert(JSON.stringify(formData));
+	var formData = myApp.formToData('#vraag_form');
+	alert(JSON.stringify(formData));
 });
-
-$$('#registreerbutton').on('click', function (){
-    var dataregistreer = myApp.formGetData('form-registreer');
-    if(dataregistreer) {
-      alert(JSON.stringify(dataregistreer));
-    }
-    else {
-      alert('There is no stored data for this form yet. Try to change any field')
-    }
+$$('#registreerbutton').on('click', function () {
+	var dataregistreer = myApp.formGetData('form-registreer');
+	if(dataregistreer) {
+		alert(JSON.stringify(dataregistreer));
+	} else {
+		alert('There is no stored data for this form yet. Try to change any field')
+	}
 });
-
-$$('#signInbutton').on('click', function (){
-  var datalogin = myApp.formGetData('form-login');
+$$('#signInbutton').on('click', function () {
+	/*var datalogin = myApp.formGetData('form-login');
   if(datalogin) {
     alert(JSON.stringify(datalogin));
   }
   else {
     alert('There is no stored data for this form yet. Try to change any field')
-  }
+}*/
+	var inputdata = {
+		function: 'login',
+		table: 'gebruiker',
+		username: $("#login-gebruikersnaam").val(),
+		wachtwoord: $("#login-wachtwoord").val(),
+	};
+	alert(inputdata);
+	$.post({
+			url: "users.php",
+			data: inputdata,
+			success: function (response) {
+				alert(response);
+			},
+			error: function (jqXHR, textStatus, errorThrown) {
+				alert(jqXHR);
+				alert(textStatus);
+				alert(errorThrown));
+		}
+	});
 });
